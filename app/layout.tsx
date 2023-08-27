7b840beff41282d86b7dfab7893fee72880f88bf
Mediocre Hacker <mediocrehacker@protonmail.com>
2023-08-27 18:01:49 +0200

    Add yandex metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@carbon/react/index.scss';
 import './global.scss';
 import { Nav } from './nav'
 import { Footer } from './Footer'
-
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Mental Desk',
@@ -22,6 +22,21 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </div>
+      <Script id="yandex-metrica" strategy="afterInteractive">
+      {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(94751413, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+        });`
+      }
+      </Script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/94751413" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
       </body>
     </html>
   )
