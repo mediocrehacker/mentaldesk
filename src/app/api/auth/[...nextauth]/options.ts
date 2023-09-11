2f340006aeb814dee3f518ff42e07ba5651e9410
Mediocre Hacker <mediocrehacker@protonmail.com>
2023-09-11 20:04:02 +0200

    add prisma

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,10 +1,15 @@
 import type { NextAuthOptions } from 'next-auth'
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import GitHubProvider from 'next-auth/providers/github'
 import YandexProvider from "next-auth/providers/yandex";
 import VkProvider from "next-auth/providers/vk";
 import CredentialsProvider from 'next-auth/providers/credentials'
 
+const prisma = new PrismaClient();
+
 export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
