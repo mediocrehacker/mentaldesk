3872fa1b319b55ff7a5e0a57d772e51703d4e72d
Mediocre Hacker <mediocrehacker@protonmail.com>
2023-09-28 21:14:13 +0200

    Refactoring

--- src/app/oprosniki/page.tsx
+++ src/app/oprosniki/page.tsx
@@ -3,32 +3,32 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter';
 
-const oprosnikiDir = path.join(process.cwd(), 'src', 'app', 'content', 'oprosniki'); 
+const surveysDir = path.join(process.cwd(), 'src', 'app', 'content', 'surveys'); 
 
-export default async function OprosnikiPage() {
-  const names = fs.readdirSync(oprosnikiDir);
+export default async function SurveysPage() {
+  const names = fs.readdirSync(surveysDir);
 
   return (
     <div className="prose prose-sm md:prose-base w-full max-w-4xl pt-8 mb-16 mx-auto">
       <h1>Опросники</h1>
           {names?.map((name) => {
-              return <Oprosnik key={name} name={name} />;
+              return <Survey key={name} name={name} />;
           })}
     </div>
   )
 }
 
-function Oprosnik({ name }: { name: string }) {
-  const file = fs.readFileSync(path.join(oprosnikiDir, `/${name}/content.mdx`));
-  const oprosnik = matter(file);
+function Survey({ name }: { name: string }) {
+  const file = fs.readFileSync(path.join(surveysDir, `/${name}/content.mdx`));
+  const survey = matter(file);
   return (
     <div>
       <h3>
         <Link className="link" href={`/oprosniki/${name}`}>
-          {oprosnik?.data?.title}
+          {survey?.data?.title}
         </Link>
       </h3>
-      <p>{oprosnik?.data?.teaser}</p>
+      <p>{survey?.data?.teaser}</p>
     </div>
   );
 }
