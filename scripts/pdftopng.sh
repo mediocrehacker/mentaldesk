a2a4e0d2045c60848da229880f87df76b31e0dbb
Mediocre Hacker <mediocrehacker@protonmail.com>
2023-10-03 21:31:57 +0200

    Generate images

--- scripts/pdftopng.sh
+++ scripts/pdftopng.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-yourfilenames=`ls ./public/surveys`
+yourfilenames=`ls ./src/app/content/surveys`
 
 for eachfile in $yourfilenames
 do
-    echo ./public/surveys/$eachfile/survey.pdf
-    if [ -f ./public/surveys/$eachfile/survey.pdf ]; then
-        pdftoppm -png ./public/surveys/$eachfile/survey.pdf ./public/surveys/$eachfile/original
-        convert ./public/surveys/$eachfile/original-1.png -resize 325x ./public/surveys/$eachfile/screenshot-1.png
+    echo ./src/app/content/surveys/$eachfile/survey.pdf
+    if [ -f ./src/app/content/surveys/$eachfile/survey.pdf ]; then
+        pdftoppm -png ./src/app/content/surveys/$eachfile/survey.pdf ./src/app/content/surveys/$eachfile/original
+        # convert ./src/app/content/surveys/$eachfile/original-1.png -resize 325x ./src/app/content/surveys/$eachfile/screenshot-1.png
     fi
 done
