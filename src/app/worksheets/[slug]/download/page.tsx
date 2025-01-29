552115216431e934f4abcac55b2f95ccb49d22fe
Mediocre Hacker <mediocrehacker@protonmail.com>
2025-01-29 23:23:46 +0100

    Remove a requirenment to be a register user for downloading

--- src/app/worksheets/[slug]/download/page.tsx
+++ src/app/worksheets/[slug]/download/page.tsx
@@ -1,16 +1,9 @@
 import { redirect } from "next/navigation";
-import { options } from "../../../api/auth/[...nextauth]/options";
-import { getServerSession } from "next-auth/next";
 
 export default async function DownloadPage(props: {
   params: Promise<{ slug: string }>;
 }) {
   const params = await props.params;
-  const session = await getServerSession(options);
   const pdfLink = `/worksheets/${params.slug}/worksheet.pdf`;
-  if (session) {
-    return redirect(pdfLink);
-  } else {
-    return redirect("/api/auth/signin");
-  }
+  return redirect(pdfLink);
 }
